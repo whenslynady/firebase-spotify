665f31ab566412f8b153d3a2400c86247188aa07
whenslynady <whenslynadycelestin0000@gmail.com>
2024-12-04 23:31:19 -0400

    Actualización de vistas y funcionalidad en Song Player, News Songs, Profile y mejoras generales

--- lib/presentation/home/widgets/news_songs.dart
+++ lib/presentation/home/widgets/news_songs.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter/services.dart'; // Para cargar y verificar imágenes en assets
+import 'package:flutter_bloc/flutter_bloc.dart'; // Para manejo de estados con Bloc
 import 'package:spotify/common/helpers/is_dark_mode.dart';
-import 'package:spotify/core/configs/constants/app_urls.dart';
 import 'package:spotify/core/configs/theme/app_colors.dart';
 import 'package:spotify/presentation/home/bloc/news_songs_cubit.dart';
 import 'package:spotify/presentation/song_player/pages/song_player.dart';
@@ -18,25 +18,22 @@ class NewsSongs extends StatelessWidget {
       create: (_) => NewsSongsCubit()..getNewsSongs(),
       child: SizedBox(
         height: 200,
-        child: BlocBuilder<NewsSongsCubit,NewsSongsState>(
-          builder: (context,state) {
+        child: BlocBuilder<NewsSongsCubit, NewsSongsState>(
+          builder: (context, state) {
             if (state is NewsSongsLoading) {
               return Container(
                 alignment: Alignment.center,
-                child: const CircularProgressIndicator()
+                child: const CircularProgressIndicator(),
               );
-            } 
+            }
 
             if (state is NewsSongsLoaded) {
-              return _songs(
-                state.songs
-              );
+              return _songs(state.songs);
             }
 
             return Container();
           },
-        )
-        
+        ),
       ),
     );
   }
@@ -45,17 +42,20 @@ class NewsSongs extends StatelessWidget {
     return ListView.separated(
       scrollDirection: Axis.horizontal,
       shrinkWrap: true,
-      itemBuilder: (context,index) {
+      itemBuilder: (context, index) {
+        // Construimos la ruta local de la imagen
+        final String imagePath =
+            'assets/images/${songs[index].artist} - ${songs[index].title}.jpg';
+
         return GestureDetector(
           onTap: () {
             Navigator.push(
               context,
               MaterialPageRoute(
-               builder: (BuildContext context) 
-              => SongPlayerPage(
-                songEntity: songs[index],
-              )
-            )
+                builder: (BuildContext context) => SongPlayerPage(
+                  songEntity: songs[index],
+                ),
+              ),
             );
           },
           child: SizedBox(
@@ -64,57 +64,82 @@ class NewsSongs extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Expanded(
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(30),
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          '${AppURLs.coverFirestorage}${songs[index].artist} - ${songs[index].title}.jpg?${AppURLs.mediaAlt}'
-                        )
-                      )
-                    ),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Container(
-                        height: 40,
-                        width: 40,
-                        transform: Matrix4.translationValues(10, 10, 0),
+                  child: FutureBuilder(
+                    future: _assetExists(imagePath),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.waiting) {
+                        return const Center(child: CircularProgressIndicator());
+                      }
+
+                      final imageExists = snapshot.data ?? false;
+
+                      return Container(
                         decoration: BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: context.isDarkMode ?  AppColors.darkGrey : const Color(0xffE6E6E6)
+                          borderRadius: BorderRadius.circular(30),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: imageExists
+                                ? AssetImage(imagePath)
+                                : const AssetImage('assets/images/default.jpg'),
+                          ),
                         ),
-                        child: Icon(
-                          Icons.play_arrow_rounded,
-                          color: context.isDarkMode ? const Color(0xff959595) : const Color(0xff555555),
+                        child: Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
+                            height: 40,
+                            width: 40,
+                            transform: Matrix4.translationValues(10, 10, 0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: context.isDarkMode
+                                  ? AppColors.darkGrey
+                                  : const Color(0xffE6E6E6),
+                            ),
+                            child: Icon(
+                              Icons.play_arrow_rounded,
+                              color: context.isDarkMode
+                                  ? const Color(0xff959595)
+                                  : const Color(0xff555555),
+                            ),
+                          ),
                         ),
-                      ),
-                    ),
+                      );
+                    },
                   ),
                 ),
-                const SizedBox(height: 10,),
+                const SizedBox(height: 10),
                 Text(
                   songs[index].title,
                   style: const TextStyle(
                     fontWeight: FontWeight.w600,
-                    fontSize: 16
+                    fontSize: 16,
                   ),
-                ) ,
-                const SizedBox(height: 5,),
+                ),
+                const SizedBox(height: 5),
                 Text(
                   songs[index].artist,
                   style: const TextStyle(
                     fontWeight: FontWeight.w400,
-                    fontSize: 12
+                    fontSize: 12,
                   ),
-                )
+                ),
               ],
             ),
           ),
         );
       },
-      separatorBuilder: (context,index) => const SizedBox(width: 14,),
-      itemCount: songs.length
+      separatorBuilder: (context, index) => const SizedBox(width: 14),
+      itemCount: songs.length,
     );
+  }
+
+  /// Método para verificar si un archivo en `assets` existe
+  Future<bool> _assetExists(String path) async {
+    try {
+      await rootBundle.load(path);
+      return true;
+    } catch (_) {
+      return false;
+    }
   }
 }
--- lib/presentation/profile/pages/profile.dart
+++ lib/presentation/profile/pages/profile.dart
@@ -6,9 +6,7 @@ import 'package:spotify/presentation/profile/bloc/favorite_songs_cubit.dart';
 import 'package:spotify/presentation/profile/bloc/favorite_songs_state.dart';
 import 'package:spotify/presentation/profile/bloc/profile_info_cubit.dart';
 import 'package:spotify/presentation/song_player/pages/song_player.dart';
-
 import '../../../common/widgets/favorite_button/favorite_button.dart';
-import '../../../core/configs/constants/app_urls.dart';
 import '../bloc/profile_info_state.dart';
 
 class ProfilePage extends StatelessWidget {
@@ -18,17 +16,15 @@ class ProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const BasicAppbar(
-        backgroundColor: Color(0xff2C2B2B) ,
-        title: Text(
-          'Profile'
-        ),
+        backgroundColor: Color(0xff2C2B2B),
+        title: Text('Profile'),
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-           _profileInfo(context),
-           const SizedBox(height: 30,),
-           _favoriteSongs()
+          _profileInfo(context),
+          const SizedBox(height: 30),
+          _favoriteSongs(),
         ],
       ),
     );
@@ -38,63 +34,50 @@ class ProfilePage extends StatelessWidget {
     return BlocProvider(
       create: (context) => ProfileInfoCubit()..getUser(),
       child: Container(
-        height: MediaQuery.of(context).size.height / 3.5 ,
+        height: MediaQuery.of(context).size.height / 3.5,
         width: double.infinity,
         decoration: BoxDecoration(
           color: context.isDarkMode ? const Color(0xff2C2B2B) : Colors.white,
           borderRadius: const BorderRadius.only(
             bottomRight: Radius.circular(50),
-            bottomLeft: Radius.circular(50)
-          )
+            bottomLeft: Radius.circular(50),
+          ),
         ),
-        child: BlocBuilder<ProfileInfoCubit,ProfileInfoState>(
-        builder: (context, state) {
-          if(state is ProfileInfoLoading) {
-            return Container(
-              alignment: Alignment.center,
-              child: const CircularProgressIndicator()
-            );
-          } 
-          if(state is ProfileInfoLoaded) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  height: 90,
-                  width: 90,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    image: DecorationImage(
-                      image: NetworkImage(
-                        state.userEntity.imageURL!
-                      )
-                    )
+        child: BlocBuilder<ProfileInfoCubit, ProfileInfoState>(
+          builder: (context, state) {
+            if (state is ProfileInfoLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            if (state is ProfileInfoLoaded) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 90,
+                    width: 90,
+                    decoration: const BoxDecoration( // Added 'const' here
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                        image: AssetImage('assets/images/default_user.png'), // Reemplaza con tu imagen predeterminada
+                      ),
+                    ),
                   ),
-                ),
-                const SizedBox(height: 15,),
-                Text(
-                  state.userEntity.email!
-                ),
-                const SizedBox(height: 10,),
-                Text(
-                  state.userEntity.fullName!,
-                  style: const TextStyle(
-                    fontSize: 22,
-                    fontWeight: FontWeight.bold
+                  const SizedBox(height: 15), // Added 'const' here
+                  Text(state.userEntity.email!),
+                  const SizedBox(height: 10), // Added 'const' here
+                  Text(
+                    state.userEntity.fullName!,
+                    style: const TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                   ),
-                )
-              ],
-            );
-          }
-
-          if(state is ProfileInfoFailure) {
-            return const Text(
-              'Please try again'
-            );
-          }
-          return Container();
-        }, 
-      ),
+                ],
+              );
+            }
+            if (state is ProfileInfoFailure) {
+              return const Text('Please try again');
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
@@ -103,35 +86,29 @@ class ProfilePage extends StatelessWidget {
     return BlocProvider(
       create: (context) => FavoriteSongsCubit()..getFavoriteSongs(),
       child: Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 16
-        ),
+        padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Text(
-              'FAVORITE SONGS',
-            ),
-            
-            const SizedBox(
-              height: 20,
-            ),
-            BlocBuilder<FavoriteSongsCubit,FavoriteSongsState>(
-              builder: (context,state) {
-                if(state is FavoriteSongsLoading) {
-                  return const CircularProgressIndicator();
+            const Text('FAVORITE SONGS'),
+            const SizedBox(height: 20),
+            BlocBuilder<FavoriteSongsCubit, FavoriteSongsState>(
+              builder: (context, state) {
+                if (state is FavoriteSongsLoading) {
+                  return const Center(child: CircularProgressIndicator());
                 }
-                if(state is FavoriteSongsLoaded) {
+                if (state is FavoriteSongsLoaded) {
                   return ListView.separated(
                     shrinkWrap: true,
                     itemBuilder: (context, index) {
                       return GestureDetector(
-                        onTap: (){
+                        onTap: () {
                           Navigator.push(
-                            context, 
+                            context,
                             MaterialPageRoute(
-                              builder: (BuildContext context) => SongPlayerPage(songEntity: state.favoriteSongs[index])
-                            )
+                              builder: (BuildContext context) =>
+                                  SongPlayerPage(songEntity: state.favoriteSongs[index]),
+                            ),
                           );
                         },
                         child: Row(
@@ -145,68 +122,57 @@ class ProfilePage extends StatelessWidget {
                                   decoration: BoxDecoration(
                                     borderRadius: BorderRadius.circular(20),
                                     image: DecorationImage(
-                                      image: NetworkImage(
-                                        '${AppURLs.coverFirestorage}${state.favoriteSongs[index].artist} - ${state.favoriteSongs[index].title}.jpg?${AppURLs.mediaAlt}'
-                                      )
-                                    )
+                                      image: AssetImage(
+                                        'assets/images/${state.favoriteSongs[index].artist} - ${state.favoriteSongs[index].title}.jpg', // Asegúrate de que el nombre del archivo coincida
+                                      ),
+                                      fit: BoxFit.cover,
+                                    ),
                                   ),
                                 ),
-                      
-                                const SizedBox(width: 10, ),
-                                  Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    children: [
-                                      Text(
-                                        state.favoriteSongs[index].title,
-                                        style: const TextStyle(
-                                          fontWeight: FontWeight.bold,
-                                          fontSize: 16
-                                        ),
-                                      ),
-                                      const SizedBox(height: 5, ),
-                                        Text(
-                                          state.favoriteSongs[index].artist,
-                                          style: const TextStyle(
-                                            fontWeight: FontWeight.w400,
-                                            fontSize: 11
-                                          ),
-                                        ),
-                                    ],
-                                  )
-                      
+                                const SizedBox(width: 10),
+                                Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      state.favoriteSongs[index].title,
+                                      style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                                    ),
+                                    const SizedBox(height: 5),
+                                    Text(
+                                      state.favoriteSongs[index].artist,
+                                      style: const TextStyle(fontWeight: FontWeight.w400, fontSize: 11),
+                                    ),
+                                  ],
+                                ),
                               ],
                             ),
                             Row(
                               children: [
-                                Text(
-                                  state.favoriteSongs[index].duration.toString().replaceAll('.', ':')
+                                Text(state.favoriteSongs[index].duration.toString().replaceAll('.', ':')),
+                                const SizedBox(width: 20),
+                                FavoriteButton(
+                                  songEntity: state.favoriteSongs[index],
+                                  key: UniqueKey(),
+                                  function: () {
+                                    context.read<FavoriteSongsCubit>().removeSong(index);
+                                  },
                                 ),
-                                const SizedBox(width: 20, ),
-                                  FavoriteButton(
-                                    songEntity: state.favoriteSongs[index],
-                                    key: UniqueKey(),
-                                    function: (){
-                                      context.read<FavoriteSongsCubit>().removeSong(index);
-                                    },
-                                )
                               ],
-                            )
+                            ),
                           ],
                         ),
                       );
                     },
-                    separatorBuilder: (context, index) => const SizedBox(height: 20,),
-                    itemCount: state.favoriteSongs.length
-                 );
-                }
-                if(state is FavoriteSongsFailure) {
-                  return const Text(
-                    'Please try again.'
+                    separatorBuilder: (context, index) => const SizedBox(height: 20),
+                    itemCount: state.favoriteSongs.length,
                   );
                 }
+                if (state is FavoriteSongsFailure) {
+                  return const Text('Please try again.');
+                }
                 return Container();
-              } ,
-            )
+              },
+            ),
           ],
         ),
       ),
